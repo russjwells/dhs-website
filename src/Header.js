51106fbf4ef5b1fbd52fae78fd4b94fa2ccb6811
Russ Wells <russ@russdigital.com>
2017-02-17 23:21:02 -0800

    pages and theme started

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
+import {Link, IndexLink} from 'react-router'
 
 class Header extends Component {
   render() {
     return (
-      <div>Header</div>
+      <div className="Header">
+        <div className="Title">
+          <IndexLink to='/' style={{ textDecoration: 'none' }} activeClassName='ActiveMenuItem' className='Title'>
+            Digital Hyperspace
+          </IndexLink>
+        </div>
+        <nav className="Menu">
+          <Link to='/projects' style={{ textDecoration: 'none' }} activeClassName='ActiveMenuItem' className='MenuItem'>
+            Projects
+          </Link>
+          <Link to='/team' style={{ textDecoration: 'none' }} activeClassName='ActiveMenuItem' className='MenuItem'>
+            Team
+          </Link>
+          <Link to='/about' style={{ textDecoration: 'none' }} activeClassName='ActiveMenuItem' className='MenuItem'>
+            About
+          </Link>
+        </nav>
+      </div>
     )
   }
 }
