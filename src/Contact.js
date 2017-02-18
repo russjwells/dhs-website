8e63f8b427cbd0e6449f85f8f5ef37791bcacf3d
Russ Wells <russ@russdigital.com>
2017-02-18 03:11:17 -0800

    contact form front end

--- src/Contact.js
+++ src/Contact.js
@@ -4,9 +4,15 @@ class Contact extends Component {
   render(){
     return(
       <div className="Contact Section">
-        <h2>Send us a message.</h2>
-        <div>
-          <form></form>
+
+        <div className="ContactForm">
+          <form>
+            <h2>Send us a message:</h2>
+            <h3>email:</h3><input type="text"/>
+            <h3>message:</h3><textarea></textarea>
+            <br/>
+            <input type="submit" value="Send"/>
+          </form>
         </div>
       </div>
     )
