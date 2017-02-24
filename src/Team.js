159c382d828bc9dea841f8ac8ecb38658b2020b9
Russ Wells <russ@russdigital.com>
2017-02-24 01:47:50 -0800

    button styles + minor edits

--- src/Team.js
+++ src/Team.js
@@ -5,7 +5,6 @@ class Team extends Component {
   render() {
     return (
       <div className="Team Section">
-        <h2>Founder</h2>
         <div className="TeamMember">
           <div className="MemberPhoto">
             <img src={russellphoto} alt="Russell" width="200" height="200"/>
@@ -13,7 +12,7 @@ class Team extends Component {
           <div className="MemberDetails">
             <h3>Russell Wells</h3>
             <p>Lead Software Engineer and Digital Designer</p>
-            <p>Russ has been creating digital products his whole life. With attention to clarity and simplicity he crafts elegant and compelling digital experiences.</p> 
+            <p>Russ has been creating digital products his whole life. With attention to clarity and simplicity he crafts elegant and compelling digital experiences.</p>
           </div>
         </div>
       </div>
