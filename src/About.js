b3e8f6c07f60477b09437771b38b44962a49f383
Russ Wells <russ@russdigital.com>
2017-03-17 05:57:09 -0700

    contact form

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ class About extends Component {
   render() {
     return (
       <div className="About Section">
-        <h2>Digital Hyperspace</h2>
+        <h2>Digital Hyperspace Manifest</h2>
         <p>Digital Hyperspace is a design and technology firm that exists to facilitate the expansion of consciousness on earth. We create applications and experiences to uplift, inspire, and evolve your human experience.</p>
       </div>
     )
--- src/Contact.js
+++ src/Contact.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react'
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'tier5'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
   render(){
     return(
       <div className="Contact Section">
         <div className="ContactForm">
           <form>
             <h2>Interested in our services?</h2>
-            <p>Your Email:</p><input type="text"/>
-            <p>Your Message:</p><textarea></textarea>
-            <br/>
+            <p>Your Email:</p>
+            <input type="text"/>
+              Your budget:
+              <select value={this.state.value} onChange={this.handleChange}>
+                <option value="tier1">less than $1000</option>
+                <option value="tier2">$1000-$2000</option>
+                <option value="tier3">$2000-$5000</option>
+                <option value="tier4">$5000-$10,000</option>
+                <option value="tier5">$10,000+</option>
+              </select>
+            <p>Your Message:</p>
+            <textarea></textarea>
+
             <input type="submit" value="Send"/>
           </form>
         </div>
