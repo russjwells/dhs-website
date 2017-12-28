e4e9c444a2faa2766625e4145e4a36d24c70565d
Russ Wells <russ@russdigital.com>
2017-12-28 10:23:42 -0800

    updated sitting project link

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -9,7 +9,7 @@ class ProjectList extends Component{
             <h2>Sitting</h2>
             <p>A multi-platform meditation timer and tracker.</p>
             <p><i>react, react native, node, express</i></p>
-            <p><a href="http://www.sittingtimer.com">Web App</a> | <a href="https://github.com/russjwells/sittingweb">Repository</a></p>
+            <p><a href="http://www.sitting.io">Web Application</a> | <a href="https://github.com/russjwells/sittingweb">Repository</a></p>
             <p><a href="https://itunes.apple.com/us/app/sitting-timer/id1139439714">iOS App</a> | <a href="https://github.com/russjwells/SittingApp">Repository</a></p>
           </div>
         </div>
